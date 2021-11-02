e5e35fc4fe30bbbd59fb2ec7c28ef0328f1cee14
berdoff99 <90041957+berdoff99@users.noreply.github.com>
2021-11-02 12:38:26 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,9 +1,17 @@
 menu.onclick = function myFunction() {
-  var x = document.getElementById('myTopnav');
-
+  var x = document.getElementById('myTopnav')
   if (x.className === "topnav") {
     x.className += " responsive"
   } else {
     x.className = "topnav";
   }
 }
+
+$(window).on("scroll", function() {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos <= 20) {
+        $('.postix').addClass('top-of-page');
+    } else {
+        $('.postix').removeClass('top-of-page');
+    }
+})
