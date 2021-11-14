6b6e0dbc84803fb55945049e77ba1c870806f3bf
berdoff99 <90041957+berdoff99@users.noreply.github.com>
2021-11-14 14:03:09 +0300

    Add files via upload

--- src/js/lanScript.js
+++ src/js/lanScript.js
@@ -47,3 +47,48 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+
+
+
+
+/* Индекс слайда по умолчанию */
+var s2lideIndex = 1;
+s2howSlides(s2lideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide2() {
+    s2howSlides(s2lideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide2() {
+    s2howSlides(s2lideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide2(m) {
+    s2howSlides(s2lideIndex = n);
+}
+
+/* Основная функция слайдера */
+function s2howSlides(m) {
+    var y;
+    var s2lides = document.getElementsByClassName("item_2");
+    var d2ots = document.getElementsByClassName("slider-dots_item_2");
+    if (m > s2lides.length) {
+      s2lideIndex = 1
+    }
+    if (y < 1) {
+        s2lideIndex = s2lides.length
+    }
+    for (y = 0; y < s2lides.length; y++) {
+        s2lides[y].style.display = "none";
+    }
+    for (y = 0; y < d2ots.length; y++) {
+        d2ots[y].className = d2ots[y].className.replace(" active", "");
+    }
+    s2lides[s2lideIndex - 1].style.display = "block";
+    d2ots[s2lideIndex - 1].className += " active";
+}
