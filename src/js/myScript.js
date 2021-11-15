7dcc1394024d624aa1505ae5a19b7c9ebdc9b8c2
berdoff99 <90041957+berdoff99@users.noreply.github.com>
2021-11-15 22:20:57 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -7,3 +7,46 @@ menu.onclick = function myFunction() {
   }
 }
 
+$(document).ready(function(){
+    
+    let options = {threshold: [0.5]};
+     let observer = new IntersectionObserver (onEntry, options);
+     let elements = $('.element-animation');
+     elements.each((i,el) => {
+         observer.observe(el);
+     });
+
+    
+    
+    
+    $("#inputTel").mask("+7(999) 999-9999");
+    
+   
+ 
+    $('form').submit(function(event){
+    if ($("#inputTel").val() == ""){
+        event.preventDefault();
+        alert("Введите телефон");
+    }
+        });
+    
+    $('form').submit(function(event){
+    if ($("#exampleInputEmail1").val() == ""){
+        event.preventDefault();
+        alert("Введите email");
+    }
+        });
+    
+      
+    
+    
+    
+});
+
+function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        }
+    });
+}
