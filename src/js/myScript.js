3ebbcfaf18ce5331193de6c37f96c6e6abcc5dea
berdoff99 <90041957+berdoff99@users.noreply.github.com>
2021-11-16 21:38:17 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -15,31 +15,45 @@ $(document).ready(function(){
      elements.each((i,el) => {
          observer.observe(el);
      });
+    
+     $('form').submit(function(event){
+        event.preventDefault();
+        
+        $.ajax({
+            type: "POST",
+            url: "php/mail.php",
+            data: $(this).serialize()
+        }).done(function (){
+            $(this).find("input").val("");
+            alert("Успешно отправлено!");
+            $("form").trigger("reset");
+        });
+        return false;
+    });
+    
+    
+   $("#inputTel").mask("+7(999) 999-9999");
 
+    $('form').submit(function(event){
+    if ($("#examplename").val() == ""){
+        event.preventDefault();
+        alert("Введите имя");
+    }
+        });
     
     
-    
-    $("#inputTel").mask("+7(999) 999-9999");
-    
-   
- 
     $('form').submit(function(event){
     if ($("#inputTel").val() == ""){
         event.preventDefault();
         alert("Введите телефон");
     }
-        });
-    
+        }); 
     $('form').submit(function(event){
     if ($("#exampleInputEmail1").val() == ""){
         event.preventDefault();
         alert("Введите email");
     }
         });
-    
-      
-    
-    
     
 });
 
@@ -50,3 +64,7 @@ function onEntry (entry){
         }
     });
 }
+
+
+
+ 
